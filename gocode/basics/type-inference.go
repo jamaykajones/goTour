a0c9ebdff712014d139257257e55f1c68379a145
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-11 10:34:41 -0400

    cool stuff

--- gocode/basics/type-inference.go
+++ gocode/basics/type-inference.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	v := 42 // change me!
+	v := 42 // variable's type is inferred from the value on the right
+	x := 42.
+	y := "42"
+	z := 0.867 + 0.5i
 	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+	fmt.Printf("z is of type %T\n", z)
 }
