4694c713635a9780d3f3cd3c8987c7cef2cb687b
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-11 09:00:18 -0400

    updated code with exe

--- gocode/basics/packages.go
+++ gocode/basics/packages.go
@@ -7,11 +7,11 @@ import (
 )
 
 func rando(min, max int) int {
-	return rand.Intn(max - min)
+	return rand.Intn(max-min) + min
 }
 
 func main() {
-	rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().Unix()) //time is always '2009-11-10 23:00:00 UTC' in Go
 	myRand := rando
 	fmt.Println("My favorite number is", myRand(1, 10))
 }
