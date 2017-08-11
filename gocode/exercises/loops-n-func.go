225781a85a1eec3215a260c1d7d2111a3e090640
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-11 12:20:00 -0400

    added math.Sqrt to compare numbers

--- gocode/exercises/loops-n-func.go
+++ gocode/exercises/loops-n-func.go
@@ -6,14 +6,14 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	for i:=0; i < 10; i++ {
-		z := float64(1)
-	    z = z - (z*z - x)/(2 *z)
+	for i:=0; i < 5; i++ {
+		z :=  float64(1)
+	    z = z - (z*z - x)/(2 *z) 
 	}
 	return x
 }
 
 func main() {
-	fmt.Println(Sqrt(16))
+	fmt.Println(Sqrt(2))
 	fmt.Printf("Sqrt of 16 is: %v Am I close?", math.Sqrt(16))
 }
