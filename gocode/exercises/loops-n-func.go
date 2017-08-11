41ce99846bc4517f618241cbbb150a108a586ce1
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-11 12:03:42 -0400

    only printing my number

--- gocode/exercises/loops-n-func.go
+++ gocode/exercises/loops-n-func.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
+	for i:=0; i < 10; i++ {
+		z := float64(1)
+	    z = z - (z*z - x)/(2 *z)
+	}
+	return x
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(16))
+	fmt.Printf("Sqrt of 16 is: %v Am I close?", math.Sqrt(16))
 }
