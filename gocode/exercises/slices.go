4afb4e1ed8f75fb4295cac24e431be70d97c5f90
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 14:16:38 -0400

    print a picture (only works in playground)

--- gocode/exercises/slices.go
+++ gocode/exercises/slices.go
@@ -3,6 +3,16 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	img := make([][]uint8, dy) //make slice length of dy
+	
+	for y := 0; y < dy; y++{
+		img[y] = make([]uint8, dx) //manipulate []uint8
+		
+		for x := 0;x < dx;x++{
+			img[y][x] = uint8((y^x)*(y^x)) //change the equation to get diff greyscale pics
+		}
+	}
+	return img
 }
 
 func main() {
