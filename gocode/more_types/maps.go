e633b041a2bf5983d95063d79cdc8a51d3ff13fe
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 14:20:46 -0400

    map literals

--- gocode/more_types/maps.go
+++ gocode/more_types/maps.go
@@ -6,12 +6,10 @@ type Dest struct {
 	Lat, Long float64
 }
 
-var m map[string]Dest
+var m = map[string]Dest{"Bell Labs": Dest{40.68433, -74.39967},
+	"Google": Dest{37.42202, -122.08408},
+}
 
 func main() {
-	m = make(map[string]Dest)
-	m["Bell Labs"] = Dest{
-		40.68433, -74.39967,
-	}
 	fmt.Println(m)
 }
