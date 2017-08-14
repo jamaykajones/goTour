7d76e49d2b5e074e0c8feb4e08d2432e2be2d54c
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 09:16:22 -0400

    struct literals

--- gocode/more_types/structs.go
+++ gocode/more_types/structs.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 type Vertex struct { //collection of fields
-	X int
-	Y int
+	X, Y int
 }
 
+var (
+	v1 = Vertex{8, 9}
+	v2 = Vertex{X: 1} //Y:0 is implicit
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v1, p, v2, v3)
 }
