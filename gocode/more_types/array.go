9a23bf30bc3e9f996783e7427f1f90f799a4c5ff
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 09:33:37 -0400

    added notes

--- gocode/more_types/array.go
+++ gocode/more_types/array.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
+	var a [2]string //array 'a' has 2 values of data
+	a[0] = "Hello"  //set first value as 'hello'
+	a[1] = "World"  //set second value as 'world'
 	fmt.Println(a[0], a[1])
 	fmt.Println(a)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	primes := [6]int{2, 3, 5, 7, 11, 13} //array named 'primes'
 	fmt.Println(primes)
 }
