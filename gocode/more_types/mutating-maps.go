9d7c5b8b9bf6aae1afff8425c5d663d201123a12
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 14:28:59 -0400

    updated code and notes

--- gocode/more_types/mutating-maps.go
+++ gocode/more_types/mutating-maps.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 	mapTest := make(map[string]int)
 
-	mapTest["kvp"] = 42
+	mapTest["kvp"] = 42 //insert or update
 	fmt.Println("The value:", mapTest["kvp"])
 
 	mapTest["kvp"] = 48
 	fmt.Println("The value:", mapTest["kvp"])
 
-	delete(mapTest, "kvp")
+	delete(mapTest, "kvp") //delete
 	fmt.Println("The value:", mapTest["kvp"])
 
-	a, ok := mapTest["kvp"]
+	a, ok := mapTest["kvp"] //test for key present
 	fmt.Println("The value:", a, "Present?", ok)
 }
