42b3021b8f7aeed8be09724ac8ed3c7cf21347dd
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-14 10:24:20 -0400

    tic-tac-toe I win!

--- gocode/more_types/slices-of-slices.go
+++ gocode/more_types/slices-of-slices.go
@@ -14,11 +14,11 @@ func main() {
 	}
 
 	// The players take turns.
-	board[0][0] = "X"
-	board[2][1] = "O"
+	board[2][0] = "X"
+	board[2][2] = "O"
 	board[1][1] = "X"
-	board[1][2] = "O"
-	board[2][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
 
 	for i := 0; i < len(board); i++ {
 		fmt.Printf("%s\n", strings.Join(board[i], " "))
