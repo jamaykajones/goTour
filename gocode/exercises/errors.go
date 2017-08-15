156159a5fd1557dfcd6567650eba063b483f8bb8
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-15 14:07:45 -0400

    errors working a ok

--- gocode/exercises/errors.go
+++ gocode/exercises/errors.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("can't Sqrt a negative number: %v", float64(e)) 
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	} else {
+		z :=  float64(1)
+		for i:=0; i < 1000; i++ {
+			z = z - (z*z - x)/(2 *z)
+		}	
+		return z, nil
+	}
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+	fmt.Println(Sqrt(16))
+	fmt.Println(Sqrt(-16))
 }
