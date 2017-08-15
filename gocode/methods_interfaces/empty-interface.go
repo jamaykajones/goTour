837313e835dd568ef53b3ac2a2d1358cc3a343bd
Jamayka Jones <jamayka.jones@safeguardproperties.com>
2017-08-15 08:44:45 -0400

    wrong copypasta, inital frfr

--- gocode/methods_interfaces/empty-interface.go
+++ gocode/methods_interfaces/empty-interface.go
@@ -2,34 +2,17 @@ package main
 
 import "fmt"
 
-type I interface {
-	M()
-}
-
 func main() {
-	var i Ipackage main
-	
-	import "fmt"
-	
-	func main() {
-		var i interface{} // empty interface
-		describe(i)
-	
-		i = 42
-		describe(i)
-	
-		i = "hello"
-		describe(i)
-	}
-	
-	func describe(i interface{}) {
-		fmt.Printf("(%v, %T)\n", i, i)
-	}
-	
+	var i interface{} //empty interface
 	describe(i)
-	i.M()
+
+	i = 42
+	describe(i)
+
+	i = "hello"
+	describe(i)
 }
 
-func describe(i I) {
+func describe(i interface{}) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
